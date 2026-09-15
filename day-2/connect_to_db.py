5cc532900082b76e108c9e605fbb917c93ce3348
jainvineet27 <vineetanything@gmail.com>
2026-09-15 07:42:28 +0200

    added connect to db

---
+++ day-2/connect_to_db.py
@@ -0,0 +1,36 @@
+import pandas as pd
+# import psycopg
+from psycopg import *
+from sqlalchemy import create_engine
+
+# postgresql+psycopg://USERNAME:PASSWORD@HOST:PORT/DATABASE
+
+engine = create_engine(
+    "postgresql+psycopg://postgres:postgres@localhost:5432/postgres"
+)
+
+def execute_query(query):
+    with engine.connect() as conn:
+        df = pd.read_sql(query, conn)
+
+    return df
+
+
+def get_schema(table_name):
+    query = f'''
+            SELECT
+    table_name ,       
+    column_name,
+    data_type
+    FROM information_schema.columns
+    where table_schema= 'public' and table_name = '{table_name}'
+    ORDER BY ordinal_position;
+        '''
+    schema = execute_query(query)
+    return schema
+
+
+result = execute_query("select * from orders limit 5")
+print(result)
+result = get_schema('orders')
+print(result)
