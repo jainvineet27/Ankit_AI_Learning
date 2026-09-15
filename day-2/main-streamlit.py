800f4dda88711c78b37aca575df29cf307f885ed
jainvineet27 <149756633+jainvineet27@users.noreply.github.com>
2026-09-15 13:56:08 +0200

    aded the new files

--- day-2/main-streamlit.py
+++ day-2/main-streamlit.py
@@ -9,8 +9,8 @@ okay so what odi havr to achivie in this project
  prompt seciton we have to make ths addition 
  
  itnegrate the app with streamlit 
- 
 '''
+
 from dotenv import load_dotenv
 import json 
 from openai import OpenAI
@@ -18,8 +18,33 @@ from connect_to_db import get_schema
 
 load_dotenv()
 
-tools  =[
+
+def get_stock_info(stock_code:str) -> str: 
+    d = {"TCS" : 1990, "INFY":200}
+
+    return str(d.get(stock_code,""))
+
+my_tools  =[
 {
+    "type": "function",
+    "name":"get_stock_info",
+    "description" :"it return the details associated with the provided stock code",
+    "parameters" :    {
+        "type":"object",
+        "properties": { 
+            "stock_code" :
+                        {
+                            "type":"string" , 
+                            "description" : "a unique code which tell stock information for a particular stock price"
+                        }
+
+        },
+        "required" :["stock_code"]
+
+    }
+}
+
+,{
     "name":"get_schema",
     "type":"function",
     "description":"get the name of the schema of the table",
@@ -37,11 +62,17 @@ tools  =[
 
 ]
 client = OpenAI()
-user_input ="giv me top 10 sales"
+
+user_input ="give me stock related information for TCS  stock code"
 response = client.responses.create(model="gpt-5.0-luna", input = user_input , tools = my_tools)
 
 
 llm_output=response.output 
+print(llm_output)
+print("======================================================================")
+
+print(response.output_text)
+
 ## it will be having some parts where it mght be making calls to the functions 
 tools_output=[]
 while True:
